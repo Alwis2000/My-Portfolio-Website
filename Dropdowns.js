c2bb95c9282bea4e72b6194c9a200ea89ed19313
Alwis2000 <theonlyalwiz2@gmail.com>
2023-05-29 07:09:26 +0530

    Major Update V4

--- Dropdowns.js
+++ Dropdowns.js
@@ -12,20 +12,3 @@ function ToggleDisplay() {
     aboutme.classList.add("CurrentItem");
   }
 }
-
-dgtitle.addEventListener("click", ToggleContainer);
-
-function ToggleContainer() {
-    
-  if (contain.classList.contains("CurrentCont")) {
-    var CurrentlyDisplayed = document.querySelectorAll(".CurrentCont");
-
-    for (var e = 0; e < CurrentlyDisplayed.length; e++) {
-      CurrentlyDisplayed[e].classList.remove("CurrentCont");
-      console.log("Removed .CurrentCont Container");
-    }
-  } else {
-    contain.classList.add("CurrentCont");
-    console.log("added .CurrentCont Container");
-  }
-}
