dae39654318e481e21f54276c0b675e41d02411b
Alwis2000 <theonlyalwiz2@gmail.com>
2023-09-01 22:48:24 +0530

    Animation

--- Parallax.js
+++ Parallax.js
@@ -1,10 +1,48 @@
 
 
-// function GalleryParallax() {
-  
-// }
-
 let mousePos = { x: undefined, y: undefined };
+
+for (let gallimg of gallimgs) {
+  let gallrect = gallimg.getBoundingClientRect();
+  let deeltaY = (gallrect.y + gallrect.height/2);
+  let deeltaX = (gallrect.x + gallrect.width/2);
+  gallimg.onmousemove = transformPanel;
+  gallimg.onmouseenter = handleMouseEnter;
+  gallimg.onmouseleave = handleMouseLeave;
+
+  let mouseX, mouseY;
+
+  let transformAmount = 1;
+
+  function transformPanel(mouseEvent) {
+    mouseX = mouseEvent.pageX;
+    mouseY = mouseEvent.pageY;
+
+    const centerX = gallimg.offsetLeft + gallimg.clientWidth / 2;
+    const centerY = gallimg.offsetTop + gallimg.clientHeight / 2;
+
+    const percentX = (mouseX - deeltaX) / (gallimg.clientWidth / 2);
+    const percentY = -((mouseY - deeltaY) / (gallimg.clientHeight / 2));
+
+    gallimg.style.transform = "perspective(400px) rotateY(" + percentX * transformAmount + "deg) rotateX(" + percentY *transformAmount + "deg)";
+  }
+
+  function handleMouseEnter() {
+    setTimeout(() => {
+        gallimg.style.transition = "";
+    }, 100);
+    gallimg.style.transition = "transform 0.1s";
+  }
+
+  function handleMouseLeave() {
+    gallimg.style.transition = "transform 0.1s";
+    setTimeout(() => {
+        gallimg.style.transition = "";
+    }, 100);
+
+    gallimg.style.transform = "perspective(400px) rotateY(0deg) rotateX(0deg)";
+  }
+}
 
 // window.addEventListener('mousemove', (event) => {
 //   for (let gallimg of gallimgs) {
@@ -13,31 +51,52 @@ let mousePos = { x: undefined, y: undefined };
 //     let deeltaY = (gallrect.y + gallrect.height/2);
 //     let deeltaX = (gallrect.x + gallrect.width/2);
 
-//     let mousePosX = event.clientX;
-//     let mousePosY = event.clientY;
+//     let mousePosX = (event.clientX - window.innerWidth/2);
+//     let mousePosY = (event.clientY - window.innerHeight/2);
 
-//     let transformAmount = 0.5;
 
-//     const centerX = gallimg.offsetLeft + window.innerWidth/2;
-//     const centerY = gallimg.offsetTop + window.innerHeight/2;
-
-//     let percentX = (mousePosX - centerX) / (deeltaX);
-//     let percentY = -((mousePosY - centerY) / (deeltaY));
-
-//     // console.log(percentX)
-
-//     gallimg.style.transform = `perspective(400px) rotateY(${percentX * transformAmount}deg) rotateX(${percentY * transformAmount}deg)`;
     
+
+    
+
+//     function transformPanel(mouseEvent) {
+//         mouseX = mouseEvent.pageX;
+//         mouseY = mouseEvent.pageY;
+
+//         const centerX = gallimg.offsetLeft + gallimg.clientWidth / 2;
+//         const centerY = gallimg.offsetTop + gallimg.clientHeight / 2;
+
+//         const percentX = (mouseX - centerX) / (gallimg.clientWidth / 2);
+//         const percentY = -((mouseY - centerY) / (gallimg.clientHeight / 2));
+
+//         gallimg.style.transform = "perspective(400px) rotateY(" + percentX * transformAmount + "deg) rotateX(" + percentY * transformAmount + "deg)";
+//     }
+
+//     function handleMouseEnter() {
+//         setTimeout(() => {
+//             gallimg.style.transition = "";
+//         }, 100);
+//         gallimg.style.transition = "transform 0.1s";
+//     }
+
+//     function handleMouseLeave() {
+//         gallimg.style.transition = "transform 0.1s";
+//         setTimeout(() => {
+//             gallimg.style.transition = "";
+//         }, 100);
+
+//         gallimg.style.transform = "perspective(400px) rotateY(0deg) rotateX(0deg)";
+//     } 
 //     // console.log(mousePosX);
 //     // gallimg.style.transform = `translate(${mousePosX*0.1}px, ${mousePosY*0.1}px)`;
     
-//     // gallimg.style.filter = `saturate(${1-Math.abs(deelta**0.5)*0.1})`;
+//     // gallimg.style.filter = `saturate(${1-Math.abs(deeltaa**0.5)*0.1})`;
 //   } 
-  
 // });
 
 
-//alt+up cool
+// alt+up cool
 
 // gallimg.style.transform = `translateY(${(gallrect.y / 10) * -1}px)`;
+
 
