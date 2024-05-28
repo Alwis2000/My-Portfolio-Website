e09b27b5d18e16c7360566eec0a8d282c7208b6e
Alwis2000 <theonlyalwiz2@gmail.com>
2024-05-28 21:02:33 +0530

    Image Resize and Mobile Features

--- SmootherScroll.js
+++ SmootherScroll.js
@@ -11,6 +11,25 @@ const options = {
       'renderByPixels' : false
     }
 
-Scrollbar.init(document.querySelector('.main'), options);
+
+
+function myFunction(Hoverable) {
+  if (Hoverable.matches) { // If media query matches
+    console.log("naur")
+  } else {
+    Scrollbar.init(document.querySelector('.main'), options);
+  }
+}
+
+// Create a MediaQueryList object
+var Hoverable = window.matchMedia("(hover: none)")
+
+// Call listener function at run time
+myFunction(Hoverable);
+
+// Attach listener function on state changes
+Hoverable.addEventListener("change", function() {
+  myFunction(Hoverable);
+});
 
 //YxN
