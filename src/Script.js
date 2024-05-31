87ca7dd9f621c2f7c138e3e69bf21910589fcedc
Alwis2000 <theonlyalwiz2@gmail.com>
2024-05-31 15:50:31 +0530

    restructure

--- src/Script.js
+++ src/Script.js
@@ -1,3 +1,27 @@
+//basic
+const cont = document.getElementsByClassName("InfoContainer")[0];
+const contovr = document.getElementsByClassName("ContainerOverlay")[0];
+//quirky
+const prof = document.getElementById("ProfileImage");
+const socT = document.getElementById("socialTwitter");
+const socD = document.getElementById("socialDiscord");
+const socR = document.getElementById("socialReddit");
+const socL = document.getElementById("socialLinkedin");
+const BGText = document.getElementById("TextClass");
+const hoverClass = document.getElementById("HoverClass");
+//aboutme
+const abouttext = document.getElementsByClassName("AboutMeText")[0];
+const aboutme = document.getElementsByClassName("AboutMe")[0];
+const aboutBod = document.getElementsByClassName("AboutBody")[0];
+const aboutbody = document.getElementById("AboutImage");
+//feedback
+const contact = document.getElementsByClassName("FeedbackText")[0];
+const contain = document.getElementsByClassName("ContainerDropdown")[0];
+const dgtitle = document.getElementById("DGTitle");
+//Gallery
+const gallimgs = document.getElementsByClassName("image");
+
+
 let social1X, social1Y, social2X, social2Y;
 let hovered = false;
 let animating = false;
